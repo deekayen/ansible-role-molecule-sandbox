9b66ee44d0df351b41ed66c67e28345fca4f2aee
David Norman <deekayen@deekayen.net>
2020-10-18 22:54:08 -0400

    Update test_default.py

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
+
 def test_apache2_installed(host):
     assert host.package("apache2").is_installed
 
